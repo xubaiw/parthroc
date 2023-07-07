703f98582c5a3b4bde208dff8847587338433a37
Xubai Wang <18016038327@189.cn>
2023-07-07 15:12:39 +0800

    Add safen

--- example.ts
+++ example.ts
@@ -6,6 +6,18 @@ export const foobar: Parser<string> = attempt((ctx) => {
   return foo + bar;
 });
 
+export const orFooBarBaz: Parser<string> = (ctx) => {
+  for (const match of ["foo", "bar", "baz"]) {
+    try {
+      const res = str(match)(ctx);
+      return res;
+    } catch {
+      continue;
+    }
+  }
+  throw `expect foo | bar | baz`;
+};
+
 if (import.meta.main) {
   const text = "fooba";
   const ctx = { text, index: 0, state: null };
--- mod.ts
+++ mod.ts
@@ -6,6 +6,30 @@ export type Context<S = unknown> = {
   state: S;
 };
 
+export type Result<T> = Success<T> | Failure;
+
+export type Success<T> = {
+  success: true;
+  value: T;
+};
+
+export type Failure = {
+  success: false;
+  reason: Reason;
+};
+
+export const SYMBOL = Symbol();
+
+export type Reason = {
+  symbol: typeof SYMBOL;
+  expected: string;
+};
+
+export const expect = (expected: string): Reason => ({
+  symbol: SYMBOL,
+  expected,
+});
+
 export const attempt = <T, S>(p: Parser<T, S>): Parser<T, S> => (ctx) => {
   const idx = ctx.index;
   try {
@@ -17,13 +41,24 @@ export const attempt = <T, S>(p: Parser<T, S>): Parser<T, S> => (ctx) => {
   }
 };
 
+export const safen = <T, S>(p: Parser<T, S>): Parser<Result<T>, S> => (ctx) => {
+  try {
+    const value = p(ctx);
+    return { success: true, value };
+  } catch (e) {
+    if (e.symbol == SYMBOL) {
+      return { success: false, reason: e };
+    }
+    throw e;
+  }
+};
+
 export const str = <S>(match: string): Parser<string, S> =>
   attempt((ctx) => {
     const endIdx = ctx.index + match.length;
     if (ctx.text.substring(ctx.index, endIdx) === match) {
       return match;
     } else {
-      throw `expect ${match}`;
+      throw expect(match);
     }
   });
-
